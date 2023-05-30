1490a2fdb7130e4dc8b1e71f1d23dc1fde2359c4
RohitNathie <103172111+RohitNathie@users.noreply.github.com>
2023-05-30 12:06:26 +0200

    random vragen op het scherm

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -30,7 +30,8 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
-
+$routes->get('/dobbelsteen', 'SpelerController::index');
+$routes->get('/vragen', 'VragenController::getVragen');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
