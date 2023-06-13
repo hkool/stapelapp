8687d04ceeaba81010b6ab1ee56dab64fa881814
PatrykPerwenis <102594009+PatrykPerwenis@users.noreply.github.com>
2023-06-13 13:27:59 +0200

    bord op scherm

--- app/Controllers/BordController.php
+++ app/Controllers/BordController.php
@@ -5,11 +5,24 @@ use App\Models\Bord;
 
 class BordController extends BaseController
 {
+    public function index()
+    {
+        return view('bord');
+    }
     public function startGame()
     {
+    
     }
     public function showBord()
     {
         return view('bord');
+    }
+    public function huidigeSpeler()
+    {
+        /*welke speler is aan de beurt*/
+    }
+    public function huidigeVraag()
+    {
+        /*welke vraag heeft de speler*/
     }
 }
